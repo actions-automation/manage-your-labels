007097f4f38e3150399346d445418d4cfb921e75
Mark Bestavros <mbestavr@redhat.com>
2020-12-16 11:35:46 -0500

    Allow arbitrary input; move example input to a separate file
    And several miscellaneous fixes.
    
    This is the first version that works.

--- src/githubgql.py
+++ src/githubgql.py
@@ -5,38 +5,6 @@ import json
 import os
 import types
 
-# TODO: remove hard-coded project IDs
-PROJECTS = {
-    'Conferences': 'MDc6UHJvamVjdDQ5OTI0MzM=',
-    'Planning': 'MDc6UHJvamVjdDQ5NjA4NDg=',
-    'Sprint': 'MDc6UHJvamVjdDQ4MjA5OTM='
-}
-
-COLUMNS = {
-    'Conferences': {
-        'Accepted': 'MDEzOlByb2plY3RDb2x1bW4xMDA1NzA1Ng==',
-        'Completed': 'MDEzOlByb2plY3RDb2x1bW4xMDA1NzA1OQ==',
-        'Considering': 'MDEzOlByb2plY3RDb2x1bW4xMDA1NzA0OQ==',
-        'Delivered': 'MDEzOlByb2plY3RDb2x1bW4xMDA1NzA1Nw==',
-        'Submitted': 'MDEzOlByb2plY3RDb2x1bW4xMDA1NzA1Mw=='
-    },
-    'Planning': {
-        'Accepted': 'MDEzOlByb2plY3RDb2x1bW4xMDAwMzg0OQ==',
-        'Assigned': 'MDEzOlByb2plY3RDb2x1bW4xMDAwODk1MQ==',
-        'Backlog': 'MDEzOlByb2plY3RDb2x1bW4xMDAwMzg0Nw==',
-        'Done': 'MDEzOlByb2plY3RDb2x1bW4xMDAwOTI3OA==',
-        'Nominated': 'MDEzOlByb2plY3RDb2x1bW4xMDAwMzg0OA==',
-        'Triage': 'MDEzOlByb2plY3RDb2x1bW4xMDAwMzg0MA=='
-    },
-    'Sprint': {
-        'Active': 'MDEzOlByb2plY3RDb2x1bW4xMDQxMTcwOA==',
-        'Assigned': 'MDEzOlByb2plY3RDb2x1bW45ODA1MjQ5',
-        'Done': 'MDEzOlByb2plY3RDb2x1bW45ODA0Mzc2',
-        'Reviewed': 'MDEzOlByb2plY3RDb2x1bW4xMDQxMTcyMg==',
-        'Reviewing': 'MDEzOlByb2plY3RDb2x1bW45ODA1MjY1'
-    }
-}
-
 class HTTPError(Exception):
     def __init__(self, reply):
         self.reply = reply
